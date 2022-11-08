90884c4c3cd899772fc21fd5c3a8af54e0e609db
darenmcgill <dmcgill@truemark.io>
2022-11-08 09:10:31 -0700

    adding outputs and updating version

--- variables.tf
+++ variables.tf
@@ -9,6 +9,54 @@ variable "az_count" {
   type = number
   default = 2
 }
+variable "network_override" {
+  type = string
+  default = "standard"
+}
+variable "private_networkbits" {
+  type = number
+  default = null
+}
+variable "public_networkbits" {
+  type = number
+  default = null
+}
+variable "intra_networkbits" {
+  type = number
+  default = null
+}
+variable "database_networkbits" {
+  type = number
+  default = null
+}
+variable "elasticache_networkbits" {
+  type = number
+  default = null
+}
+variable "redshift_networkbits" {
+  type = number
+  default = null
+}
+variable "public_netnum" {
+  type = number
+  default = null
+}
+variable "intra_netnum" {
+  type = number
+  default = null
+}
+variable "database_netnum" {
+  type = number
+  default = null
+}
+variable "elasticache_netnum" {
+  type = number
+  default = null
+}
+variable "redshift_netnum" {
+  type = number
+  default = null
+}
 # TODO Likely shouldn't be a number. This should be three options, SINGLE_AZ, MULTI_AZ, POOR_PERSON_NAT, OR NONE
 variable "nat_type" {
   type = string
@@ -44,8 +92,27 @@ variable "redshifttags" {
   type = map(string)
   default = null
 }
-
 variable "redshift" {
   type = bool
   default = false
+}
+variable "intra" {
+  type = bool
+  default = true
+}
+variable "database" {
+  type = bool
+  default = true
+}
+variable "elasticache" {
+  type = bool
+  default = true
+}
+variable "private" {
+  type = bool
+  default = true
+}
+variable "public" {
+  type = bool
+  default = true
 }
